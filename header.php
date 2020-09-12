79aa78e90e0c5db6f799f2f04937f92a8e3c0061
Roop Sannigrahi <roopsannigrahi@gmail.com>
2020-09-12 21:07:17 +0530

    all files added

---
+++ header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+  <head>
+     <meta charset="utf-8">
+     <meta name="description" content="This is an example of a meta description. This will often show up in search results.">
+     <meta=viewport content="width=device-width, initial-scale=1">
+     <title></title>
+     <!--<link rel="stylesheet" href="style.css">-->
+   </head>
+   <body>
+
+   <header>
+        <nav>
+          <a href="#">
+            <img src="home/roop/website/logo.jpg" alt="logo">
+          </a>
+          <ul>
+            <li><a href="#index.php">Home</a></li>
+            <li><a href="#">Portfolio</a></li>
+            <li><a href="#">About me</a></li>
+            <li><a href="#">Contact</a></li>
+          </ul>
+          <div>
+           <form action="includes/login.inc.php" method="post">
+            
+           </form>
+          </div>
+        </nav>
+
+   </header>
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+require "header.php";
+?>
+
+<main>
+  <div class="wrapper-main">
+    <section class="section-default">
+      <p>Your are logged out!</p>
+      <p>You are logged in!</p>
+    </section>
+  </div>
+</main>
+
+<?php
+require "footer.php";
+?>
+
